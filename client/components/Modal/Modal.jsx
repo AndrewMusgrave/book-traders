c7da80e09576c021bf4aa8d143b242be05b2c016
Andrew Musgrave <andrewdmusgrave@gmail.com>
2018-06-08 10:09:35 -0400

    Added modal files

---
+++ client/components/Modal/Modal.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import * as styles from './Modal.scss';
+
+class Modal extends Component {
+    render() {
+        return null;
+    }
+}
+
+export default Modal;
